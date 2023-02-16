aab1f2a7f8300690d7b031c0ab61dedc79356efe
Eugenio Pastoral <gino@achrovisual.com>
2023-02-16 13:46:04 +0800

    Added filename to image model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_29_045554) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_16_054409) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,6 +26,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_29_045554) do
   create_table "images", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "filename"
   end
 
   create_table "projects", force: :cascade do |t|
